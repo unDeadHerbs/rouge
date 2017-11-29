be3efe712c770356a6488a6f897d5d213883aba5
unDeadHerbs <undeadherbs@gmail.com>
2017-11-28 21:08:13 -0600

    set debugging verbosity

--- defines.hpp
+++ defines.hpp
@@ -1,4 +1,7 @@
-#ifndef __DEFINES
-#define __DEFINES
-#define DEBUGGER 1
+#ifndef __DEFINES__
+#define __DEFINES__
+	#define DEBUGGER 1
+	#define DEBUGLOG 1
+	#define DEBUG 1
+	#define VERBOSE 5
 #endif
