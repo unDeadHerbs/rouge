8da351514d8e657ff8d8a4e0183f8bdbdeb47523
unDeadHerbs <undeadherbs@gmail.com>
2017-11-28 21:08:47 -0600

    made a slightly longer test case to see text being drawn

--- main.cpp
+++ main.cpp
@@ -1,3 +1,8 @@
+#include <string>
+using std::string;
+#include <deque>
+using std::deque;
+
 #include "udh_ncurses.hpp"
 #include "debug.hpp"
 
@@ -5,6 +10,12 @@ int main(){
   debug("starting");
   udh_screen Screen;
   usleep(1*1000*1000);
-  
+  deque<string> new_rows;
+  new_rows.push_back("row one");
+  new_rows.push_back("row two");
+  Screen.drawToScreen(new_rows);
+  usleep(3*1000*1000);
+  Screen.updateScreen();
+  usleep(3*1000*1000);
   return 0;
 }
