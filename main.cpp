6ac0bb6504f956243ad6a73c753f5211937ecdf2
unDeadHerbs <undeadherbs@gmail.com>
2017-11-30 21:07:12 -0600

    updated test for working on cursor

--- main.cpp
+++ main.cpp
@@ -16,18 +16,24 @@ int main() {
 	new_rows.push_back("row one");
 	new_rows.push_back("row two");
 	Screen.drawToScreen(new_rows);
-	usleep(3 * 1000 * 1000);
+	usleep(1 * 1000 * 1000);
 	Screen.refreshScreen();
-	usleep(3 * 1000 * 1000);
+	usleep(1 * 1000 * 1000);
 
 	deque<string> test2;
 	test2.push_back("this is");
 	test2.push_back("some text");
 	test2.push_back("row three");
 	Screen.drawToScreen(test2, 20, 20);
-	usleep(3 * 1000 * 1000);
+	usleep(1 * 1000 * 1000);
+	Screen.refreshScreen();
+	usleep(1 * 1000 * 1000);
+
+	deque<string> prompt;
+	prompt.push_back("type here:");
+	Screen.drawToScreen(prompt, 20,0);
+	Screen.cursor= std::make_pair(20,prompt[0].size());
 	Screen.refreshScreen();
 	usleep(3 * 1000 * 1000);
-
 	return 0;
 }
