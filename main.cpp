1b70ae5cedfafacd57587c9a16dd31d1a8c04927
unDeadHerbs <undeadherbs@gmail.com>
2017-12-01 02:42:47 -0600

    made simple movement commands to build off of

---
+++ main.cpp
@@ -0,0 +1,55 @@
+#include "debug.hpp"
+#include "udh_frame.hpp"
+#include "udh_ncurses.hpp"
+
+int main() {
+	udh_screen Screen;
+	auto &main_frame= Screen.frame();
+
+	udh_frame map; // make a constructor for this
+	map.add_row("+-------+ +-------+");
+	map.add_row("|       | |       |");
+	map.add_row("|       | |       |");
+	map.add_row("|       | |       |");
+	map.add_row("+--+ +--+ +--+ +--+");
+	map.add_row("   | |       | |   ");
+	map.add_row("+--+ +-------+ +--+");
+	map.add_row("|                 |");
+	map.add_row("+-----------------+");
+
+	main_frame.place_frame(map);
+
+	std::pair<uint, uint> me= {2, 4};
+
+	char ch;
+	do {
+		main_frame.place_frame(map);
+		main_frame[me.first][me.second]= '@';
+
+		Screen.refreshScreen();
+		ch= Screen.getKey();
+		debugv(2, "got key " + std::to_string(ch));
+		switch (ch) {
+		case 'w':
+			if (map[me.first - 1][me.second] == ' ') me.first--;
+			break;
+		case 'a':
+			if (map[me.first][me.second - 1] == ' ') me.second--;
+			break;
+		case 's':
+			if (map[me.first + 1][me.second] == ' ') me.first++;
+			break;
+		case 'd':
+			if (map[me.first][me.second + 1] == ' ') me.second++;
+			break;
+		case 'q':
+			break;
+			debugv("quiting");
+			break;
+		default:
+			break;
+		}
+	} while (ch != 'q');
+
+	return 0;
+}
