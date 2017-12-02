2da79c8974f31e0268e4146fce8d5525005aa5fa
unDeadHerbs <undeadherbs@gmail.com>
2017-12-02 13:39:11 -0600

    created status message

--- game.hpp
+++ game.hpp
@@ -12,30 +12,46 @@ private:
   udh_frame* main_frame;
   RRMap map;
   uint view_size;
+  std::string status;
+  uint status_length;
 public:
   bool running=true;
   RRGame(udh_frame* mf):main_frame(mf){
     view_size=7;
+    status_length=40;
     player=map.getSpawn();
     main_frame->place(map.view_port(player,view_size));
+    main_frame->place("Status: " + status,view_size*2+3);
   }
   void handel_key(char ch){
     switch (ch) {
     case 'w':case 'W':
-      if (map[player.first - 1][player.second] == ' ')
+      if (map[player.first - 1][player.second] == ' '){
 	player.first--;
+      	status="";
+	  }else
+	status="That's a wall!";
       break;
     case 'a':case 'A':
-      if (map[player.first][player.second - 1] == ' ')
+      if (map[player.first][player.second - 1] == ' '){
 	player.second--;
+	status="";
+		  }else
+	status="That's a wall!";
       break;
     case 's':case 'S':
-      if (map[player.first + 1][player.second] == ' ')
+      if (map[player.first + 1][player.second] == ' '){
 	player.first++;
+	status="";
+      }else
+	status="That's a wall!";
       break;
     case 'd':case 'D':
-      if (map[player.first][player.second + 1] == ' ')
+      if (map[player.first][player.second + 1] == ' '){
 	player.second++;
+      status="";
+      }else
+	status="That's a wall!";
       break;
     case 'q':case 'Q':
       running=false;
@@ -45,6 +61,8 @@ public:
       break;
     }
     main_frame->place(map.view_port(player,view_size));
+    main_frame->place("Status:" + std::string(status_length,' '),view_size*2+3);
+    main_frame->place("Status:" + status,view_size*2+3);
   }
 };
 
